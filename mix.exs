6e198f8b2f553b5b9c9e12f271fd73ab4d05c6e4
CrowdHailer <peterhsaxton@gmail.com>
2018-04-22 19:02:14 +0100

    update for release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ServerSentEvent.Mixfile do
 
   def project do
     [app: :server_sent_event,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
