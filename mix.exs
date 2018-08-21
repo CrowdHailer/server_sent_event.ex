1383c7da636ce573fac7c2e174f2acbbd6f40b35
CrowdHailer <peterhsaxton@gmail.com>
2018-08-21 20:18:26 +0100

    only test supported versions, i.e. from OTP 21

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ServerSentEvent.Mixfile do
     [
       app: :server_sent_event,
       version: "0.3.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
